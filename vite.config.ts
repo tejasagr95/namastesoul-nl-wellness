01d05697a7ab456f9ae5158f51343cd949d7437f
tejas agrawal <tejas.agrawal@coolblue.nl>
2025-09-01 21:55:22 +0200

    Updated .github deploy files

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/namastesoul-nl-wellness/",
   server: {
     host: "::",
     port: 8080,
